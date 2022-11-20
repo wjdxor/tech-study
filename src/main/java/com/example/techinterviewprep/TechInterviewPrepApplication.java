c0c5d1fffa238240900cec5e2754dc9312f8d561
JT <38105420+wjdxor@users.noreply.github.com>
2022-11-20 21:45:32 +0900

    init: new project

---
+++ src/main/java/com/example/techinterviewprep/TechInterviewPrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.techinterviewprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechInterviewPrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechInterviewPrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techinterviewprep/TechInterviewPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techinterviewprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechInterviewPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
